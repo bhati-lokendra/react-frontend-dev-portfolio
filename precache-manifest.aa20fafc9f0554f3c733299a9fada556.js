e30e789d72c66a626f8af4be4c6ebcabd587e2d7
Lokendra Singh <lokendrasinghb11@gmail.com>
2025-07-12 22:38:44 +0530

    Updates

---
+++ precache-manifest.aa20fafc9f0554f3c733299a9fada556.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c51511125101ef805f45b5862aa6e16",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "4c655ee376e6920f7238",
+    "url": "/react-frontend-dev-portfolio/static/css/2.34178e39.chunk.css"
+  },
+  {
+    "revision": "abe58e5fb96a7970b1e2",
+    "url": "/react-frontend-dev-portfolio/static/css/main.0d82120f.chunk.css"
+  },
+  {
+    "revision": "4c655ee376e6920f7238",
+    "url": "/react-frontend-dev-portfolio/static/js/2.9f999402.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.9f999402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe58e5fb96a7970b1e2",
+    "url": "/react-frontend-dev-portfolio/static/js/main.e2e415b0.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
